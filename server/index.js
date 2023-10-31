5a084491cb98dbbd59c5eef81f63435b5f14f28f
Maryo Botros <mbotros1912@gmail.com>
2023-10-30 20:38:10 -0700

    Added a post request function

--- server/index.js
+++ server/index.js
@@ -12,18 +12,36 @@ const ActivityModel = require("./models/Activity");
 // Connect the database to MongoDB
 mongoose.connect("mongodb+srv://user123:Password123@cluster0.hzjlqtm.mongodb.net/todolist?retryWrites=true&w=majority");
 
-// Get all the Activities
+
+// GET all the Activities
 // URL: http://localhost:3001/getActivities
 app.get("/getActivities", async (req, res) => {
-    try{
+    try {
         // Construct the query
         const query = {};
 
         // Search the database
-        const friends = await Activitymodel.find(query);
+        const friends = await ActivityModel.find(query);
 
-        // Construct the query
+        // Construct the response and send it back
         res.json(friends);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// POST a new activity
+// URL: http://localhost:3001/insertActivity
+app.post("/insertActivity", async (req, res) => {
+    try {
+        // Create a new activity based on what's passed into the bodt and save it
+        const activity = req.body;
+        const newActivity = ActivityModel(activity);
+        await newActivity.save();
+
+        // Construct the response and send it back
+        res.json(newActivity);
     } catch (err) {
         res.status(500).json(err);
     }
